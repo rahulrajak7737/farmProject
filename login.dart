7fb5a1cb3e1d70761d45939af42f9ed553203f0d
rahulrajak7737 <71347446+rahulrajak7737@users.noreply.github.com>
2023-04-06 15:48:38 +0530

    Update login.dart

--- login.dart
+++ login.dart
@@ -10,240 +10,111 @@ class Mylogin extends StatefulWidget {
 class _MyloginState extends State<Mylogin> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
+    return Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/p2.jpg'),
+            fit: BoxFit.cover
+          )
+
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("Welcome to Farmtech",
-              style: TextStyle(
-                fontFamily: 'Poppins',
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
+             Row(
+               mainAxisSize: MainAxisSize.max,
+               mainAxisAlignment: MainAxisAlignment.center,
+               crossAxisAlignment: CrossAxisAlignment.center,
+               children: [
+                 Text('Welcome to Farmtech', style: TextStyle(
+                   fontSize: 24,
+                   fontWeight: FontWeight.w800,
+                 ),)
+               ],
+             ),
+              SingleChildScrollView(
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height*0.25,
+                  color: Colors.white70,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'User Name',
+                          fillColor: Colors.white70,
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)
+                          )
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: 'Password',
 
-              ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.15,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade100,
+                            fillColor: Colors.white70,
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)
+                            )
+                        ),
+                      ),
+
+                        ],
+                      ),
                 ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Row(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(
-                                  25, 25, 25, 25),
-
-                            child: Text("User Name",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
-                            ),
-                            ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Padding(
-                              padding: EdgeInsetsDirectional.fromSTEB(
-                                  25, 25, 25, 25),
-
-                              child: Text("Password",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              ),
-                            ),
-                          ],
-
-                        ),
-                        Column(
-                          children:[
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                Padding(padding: EdgeInsetsDirectional.fromSTEB(15, 15, 15, 15),
-
-                               child: Container(
-                                 width: MediaQuery.of(context).size.width * 0.55,
-                                 height: MediaQuery.of(context).size.height * 0.05,
-                                   decoration: BoxDecoration(
-                                     borderRadius: BorderRadius.circular(15),
-                                     border: Border.all(
-                                       width: 1,
-                                     ),
-                                   ),
-                                 child: TextField(
-                                  autofocus: true,
-                                   obscureText: false,
-                                   decoration: InputDecoration(
-                                     enabledBorder: UnderlineInputBorder(
-                                       borderSide: BorderSide(
-                                         color: Color(0x00000000),
-                                         width: 1,
-                                       ),
-                                       borderRadius: const BorderRadius.only(
-                                         topLeft: Radius.circular(4.0),
-                                         topRight: Radius.circular(4.0),
-                                       ),
-                                     ),
-                                     focusedBorder: UnderlineInputBorder(
-                                       borderSide: BorderSide(
-                                         color: Color(0x00000000),
-                                         width: 1,
-                                       ),
-                                       borderRadius: const BorderRadius.only(
-                                         topLeft: Radius.circular(4.0),
-                                         topRight: Radius.circular(4.0),
-                                       ),
-                                     ),
-                                     errorBorder: UnderlineInputBorder(
-                                       borderSide: BorderSide(
-                                         color: Color(0x00000000),
-                                         width: 1,
-                                       ),
-                                       borderRadius: const BorderRadius.only(
-                                         topLeft: Radius.circular(4.0),
-                                         topRight: Radius.circular(4.0),
-                                       ),
-                                     ),
-                                     focusedErrorBorder: UnderlineInputBorder(
-                                       borderSide: BorderSide(
-                                         color: Color(0x00000000),
-                                         width: 1,
-                                       ),
-                                       borderRadius: const BorderRadius.only(
-                                         topLeft: Radius.circular(4.0),
-                                         topRight: Radius.circular(4.0),
-                                       ),
-                                     ),
-                                   ),
-                                 ),
-
-                               ),
-                                ),
-                              ],
-
-                          ),
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                Padding(padding: EdgeInsetsDirectional.fromSTEB(15, 15, 15, 15),
-
-                                  child: Container(
-                                    width: MediaQuery.of(context).size.width * 0.55,
-                                    height: MediaQuery.of(context).size.height * 0.05,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(15),
-                                      border: Border.all(
-                                        width: 1,
-                                      ),
-                                    ),
-                                    child: TextField(
-                                      autofocus: true,
-                                      obscureText: false,
-                                      decoration: InputDecoration(
-                                        enabledBorder: UnderlineInputBorder(
-                                          borderSide: BorderSide(
-                                            color: Color(0x00000000),
-                                            width: 1,
-                                          ),
-                                          borderRadius: const BorderRadius.only(
-                                            topLeft: Radius.circular(4.0),
-                                            topRight: Radius.circular(4.0),
-                                          ),
-                                        ),
-                                        focusedBorder: UnderlineInputBorder(
-                                          borderSide: BorderSide(
-                                            color: Color(0x00000000),
-                                            width: 1,
-                                          ),
-                                          borderRadius: const BorderRadius.only(
-                                            topLeft: Radius.circular(4.0),
-                                            topRight: Radius.circular(4.0),
-                                          ),
-                                        ),
-                                        errorBorder: UnderlineInputBorder(
-                                          borderSide: BorderSide(
-                                            color: Color(0x00000000),
-                                            width: 1,
-                                          ),
-                                          borderRadius: const BorderRadius.only(
-                                            topLeft: Radius.circular(4.0),
-                                            topRight: Radius.circular(4.0),
-                                          ),
-                                        ),
-                                        focusedErrorBorder: UnderlineInputBorder(
-                                          borderSide: BorderSide(
-                                            color: Color(0x00000000),
-                                            width: 1,
-                                          ),
-                                          borderRadius: const BorderRadius.only(
-                                            topLeft: Radius.circular(4.0),
-                                            topRight: Radius.circular(4.0),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-
-                                  ),
-                                ),
-                              ],
-
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-          Padding(padding: EdgeInsetsDirectional.fromSTEB(25, 25, 25, 25),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                  onPressed:  () {
-                    print('Button pressed ...');
-                  },
-                  child: Text(
-                    "Log In",
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                      onPressed: (){}, child: Text('Sign In',
                     style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Poppins',
-
+                      fontSize: 18,
                     ),
 
                   ),),
+                  TextButton(
+                    onPressed: (){
+                      Navigator.pushNamed(context,'role');
+                    }, child: Text('Sign Up',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+
+                  ),),
+                ],
+              ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              TextButton(
+              onPressed: (){}, child: Text('Forgot Password ? ',
+          style: TextStyle(
+            fontSize: 18,
+          ),
+              ),
+
+                ),
+              ],
+    ),
             ],
           ),
-          ),
-        ],
-      ),
-
-
-  
-
-
+        ),
     );
+
+
+
   }
 }
